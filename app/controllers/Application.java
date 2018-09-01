5e6aceca65beb3ae399f9f3bd879d3a2d9bb728d
Tarik <tarik.boukarache@gmail.com>
2018-09-02 00:32:59 +0100

    add sysout test

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,11 +5,11 @@ import play.mvc.*;
 
 import java.util.*;
 
-import models.*;
 
 public class Application extends Controller {
 
     public static void index() {
+        System.out.print("test !!");
         render();
     }
 
